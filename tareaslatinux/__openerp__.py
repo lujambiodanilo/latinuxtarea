ceb734b797c09e64e37cc188591eb6e0fa904bd0
lujambiodanilo <lujambiodanilo@gmail.com>
2013-04-13 19:33:43 -0300

    Primera incorporación de archivos contiene una estructura básica de latinuxtarea y el comienzo de latinuxtarea_recursos que es la clase que representará a los recursos utilizados por tareas

---
+++ tareaslatinux/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'tareaslatinux',
+    'version': '0.1',
+    'category': 'Tools',
+    'description': """
+Una primera versión de módulo para manejar tareas en Latinux
+""",
+    'author': 'Latinux Sistemas',
+    'website': '',
+    'depends': ['base'],
+    'data': [
+        'tareaslatinux_view.xml',
+    ],
+    'demo': [],
+    'test':[],
+    'installable': True,
+    'images': [],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
